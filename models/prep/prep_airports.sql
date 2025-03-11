dd4861725f9ea2b6b071ac741c27f797b104d176
Apocaglarr <caglar.abdullah@yahoo.de>
2025-03-11 16:12:36 +0100

    airport

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,6 @@
+WITH airports_reorder AS (  SELECT faa
+    	   , country, region, city, name, lat, lon, alt, tz, dst
+    	   
+    FROM {{ref('staging_airports')}}
+)
+SELECT * FROM airports_reorder
